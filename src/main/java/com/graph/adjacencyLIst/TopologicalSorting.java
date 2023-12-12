1beb9d00bb370c100b2a3b7159fd620e4f79e30a
Suhag2001 <suhagmohinkar100@gmail.com>
2023-12-12 16:38:34 +0530

    adjacecy list type of graph inserted

---
+++ src/main/java/com/graph/adjacencyLIst/TopologicalSorting.java
@@ -0,0 +1,10 @@
+package com.graph.adjacencyLIst;
+
+//Directed acyclic graph(DAG) is a directed graph with no cycles.
+// Topological sorting is used noly for DAGs and not for non-DAGs.
+//It is a linear order of vertices such that every directed edge u->v, the vertex u comes before v in the order.
+
+
+public class TopologicalSorting {
+
+}
